4cc725d26c8c29e87f008100b0dea228b104723b
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-10-06 17:21:44 +0200

    header big screen done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ import logo from "../images/logo.jpg";
 const Header = () => {
 	const [display, setDisplay] = useState("");
 	const [hideButton, setHideButton] = useState("");
+	const [hideNav, setHideNav] = useState("");
+	const [showMenu, setShowMenu] = useState("menu__hidden");
 	const displayMenu = () => {
 		if (display === "displayed") {
 			setDisplay("");
@@ -14,13 +16,43 @@ const Header = () => {
 			setHideButton("hidden");
 		}
 	};
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 150) {
+			setHideNav("nav__hidden");
+			setShowMenu("menu__displayed");
+		} else {
+			setHideNav("");
+			setShowMenu("menu__hidden");
+		}
+		// if(offset > 150) {
+		//   header.style.transform = 'translateY(-70%)';
+		//   para.style.display = 'block';
+		// } else {
+		//   header.style.transform = 'translateY(0)';
+		//   para.style.display = 'none';
+		// }
+	});
+	window.addEventListener("mousemove", (e) => {
+		let client = e.clientY;
+		if (window.scrollY > 200 && client < 50) {
+			setHideNav("");
+		} else if (window.scrollY > 200 && client > 50) {
+			setHideNav("nav__hidden");
+		}
+		//console.log("Y: " + client);
+		// if(offset > 200 && client < 50) {
+		//   header.style.transform = 'translateY(0)';
+		// } else if(offset > 200 && client > 50) {
+		//   header.style.transform = 'translateY(-70%)';
+		// }
+	});
 	return (
 		<div className="section header__section ">
 			<img src={logo} alt="Logo Robin Lepoutre" />
 			<div className={hideButton + " header__btn"}>
 				<button onClick={displayMenu}>Menu</button>
 			</div>
-			<header className={display}>
+			<header className={display + " " + hideNav}>
 				<nav>
 					<span className="header__close--menu" onClick={displayMenu}>
 						X
@@ -49,6 +81,7 @@ const Header = () => {
 						</li>
 					</ul>
 				</nav>
+				<p className={showMenu}>Menu</p>
 			</header>
 		</div>
 	);
