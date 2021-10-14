d3385b0e0399de93cc6a9c049818733e0bd0dd13
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-10-14 18:43:37 +0200

    mentions legales done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,30 @@ const Footer = () => {
 	return (
 		<div className="footer">
 			<div className="footer__flex">
-				<Link to="/mentions-legales">Mentions légales</Link>
+				<div className="footer__legals">
+					<Link to="/mentions-legales">
+						Mentions légales
+						<span className="footer__underline"></span>
+					</Link>
+				</div>
 				<div className="footer__socials">
-					<span>facebook / </span>
-					<span>instagram</span>
+					<a
+						href="https://www.facebook.com/profile.php?id=100046316814416"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Facebook
+						<span className="footer__underline"></span>
+					</a>
+					&nbsp;&nbsp;&nbsp;
+					<a
+						href="https://www.instagram.com/robin.lepoutre/?r=nametag&fbclid=IwAR35PLcU9gssikw4fS32w0XMjdUH6gdr1bqZJ36YutR16fCIcuFQVlw0Pak"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Instagram
+						<span className="footer__underline"></span>
+					</a>
 				</div>
 			</div>
 		</div>
