17f1c4dd867924412d35237a45578d3950254a46
Gontran-de-Kermadec <gontrandekermadec@gmail.com>
2024-05-04 12:27:38 -0400

    landing page new logo

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import "../style/Landing.css";
+import robin from "../images/robin_logo.jpg";
 const LandingPage = () => {
 	return (
 		<div className="landing">
-			<h1>Robin LEPOUTRE</h1>
+			{/* <h1>Robin LEPOUTRE</h1> */}
+			<div className="img__container">
+				<img src={robin} alt="Robin en train de tatouer" />
+			</div>
 			<section className="button__container">
 				<button>
 					<a href="https://www.titonoart.com/" target="_blank" rel="noreferrer">
